e7a5a7d803e52bb58bfb409b3965ae34a3746d8c
Plinio Villas Boas <pliniovillas@gmail.com>
2025-02-21 19:18:39 -0300

    Delete sql-server-app_sql-server-app directory

--- sql-server-app_sql-server-app/src/main/java/com/sql/server/sql_server_app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sql.server.sql_server_app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sql.server.sql_server_app.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{}
